fee7080d9f9d9a349098afd0763dfc0cdeca4bbc
Maciej Kapusta <maciej.kapusta@tesco.com>
2024-07-12 09:33:33 +0200

    Better version

--- main.go
+++ main.go
@@ -7,35 +7,52 @@ import (
 )
 
 func main() {
-	fmt.Println("Jak masz na imię?")
-	prompt()
-	var name string
-	fmt.Scanln(&name)
+	name := readValue("Jak masz na imię?")
+
 	fmt.Println("Witaj", name)
 	fmt.Println("Pomóż nam uratować Kraków")
-	for i := 0; i < 2; i++ {
-
-		first := rand.Int63n(10)
-		second := rand.Int63n(10)
+	count := rand.Intn(10)
+	fmt.Println("Zadam Ci", count, "pytań")
+	good := 0
+	bad := 0
+	for i := 0; i < count; i++ {
+		first := randomMultiplier()
+		second := randomMultiplier()
+		result := getAnswer(first, second)
 		actualResult := first * second
-		fmt.Println("Ile jest", first, "razy", second, "?")
-		prompt()
-		var answer string
-		fmt.Scanln(&answer)
+		if result == actualResult {
+			fmt.Println("Dobrze!")
+			good++
+		} else {
+			fmt.Println("Prawidłowa odpowiedź to", actualResult)
+			bad++
+		}
+	}
+
+	fmt.Println("Udzielono", good, "odpowiedzi poprawnych, oraz", bad, "złych.")
+
+}
+
+func randomMultiplier() int64 {
+	return rand.Int63n(9) + 1
+}
+
+func getAnswer(first int64, second int64) int64 {
+	for {
+		answer := readValue("Ile jest", first, "razy", second, "?")
 		result, err := strconv.ParseInt(answer, 10, 32)
 		if err != nil {
 			fmt.Println("Nie rozumiem")
-		} else {
-			if result == actualResult {
-				fmt.Println("Dobrze!")
-			} else {
-				fmt.Println("Prawidłowa odpowiedź to", actualResult)
-			}
+			continue
 		}
-
+		return result
 	}
 }
 
-func prompt() {
+func readValue(question ...any) string {
+	var answer string
+	fmt.Println(question...)
 	fmt.Print("> ")
+	_, _ = fmt.Scanln(&answer)
+	return answer
 }
